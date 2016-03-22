c3b5911371a7878c2e01df37df2394cf511f5bd2
apatel0708 <apatel0708@gmail.com>
2016-03-22 17:20:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,7 +47,7 @@ function respond() {
   } 
   else if(request.text && botRegexAd.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.hackcollege.com/wp-content/uploads/2013/02/kno_advance.jpg");
+    postMessage("https://www.daddyleagues.com/fbos53#advance");
     this.res.end();
   }
   else if(request.text && botRegexRules.test(request.text)) {
